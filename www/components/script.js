852ce4a9b3b95a996c4394f66ab10dfd6abfb602
Apolloyy <luroyaaal@gmail.com>
2023-03-11 06:34:54 +0900

    Implementando estilização, função que calcula a regra de três e responsividade

--- www/components/script.js
+++ www/components/script.js
@@ -1,8 +1,12 @@
-function caucular(){
-var a;
-var b;
-var c;
-var d;
+function calcule() {
 
+var primeiro = document.getElementById("v1").value;
+var segundo = document.getElementById("v2").value;
+var terceiro = document.getElementById("v3").value;
 
+	var resultado = (primeiro*segundo)/terceiro;
+	console.log(resultado);
+
+	var print = document.getElementById("caixa");
+	print.innerHTML = "O Resultado é: " + resultado;
 }
